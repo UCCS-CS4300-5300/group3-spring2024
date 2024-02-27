a80e6426f198460c3442a503f79b0b9b9b670dec
rbieg <rbieg@uccs.edu>
2024-02-26 21:01:17 -0700

    added flight_search.html and functionality to search by price, departure location, and departure time

--- flightcomparison/urls.py
+++ flightcomparison/urls.py
@@ -6,4 +6,5 @@ from . import views
 appName = 'flightcomparison'
 urlpatterns = [
     path('', views.home, name='home'),
+    path('flight_search/', views.flight_search, name='flight_search'),
 ]
--- flightcomparison/views.py
+++ flightcomparison/views.py
@@ -2,8 +2,10 @@ from django.db.models import indexes
 from django.shortcuts import get_object_or_404, render, redirect
 from django.http import HttpResponse
 from django.template import loader
+from django.views import generic
 import json
 from . import serializers
+from .models import *
 
 '''
 General Views
@@ -14,3 +16,21 @@ def home(request):
   binary = response.content
   data = json.loads((binary).decode())
   return render(request, 'home.html', {'flights': data['data']})
+
+def flight_search(request):
+    if request.method == 'GET':
+        departure_location = request.GET.get('departure_location', '')
+        departure_time = request.GET.get('departure_time', '')
+        price = request.GET.get('price', '')
+
+        flights = Flight.objects.all()
+        if departure_location:
+            flights = flights.filter(departure_location__icontains=departure_location)
+        if departure_time:
+            flights = flights.filter(departure_time=departure_time)
+        if price:
+            flights = flights.filter(price=price)
+
+        return render(request, 'flight_search.html', {'flights': flights})
+    else:
+        return render(request, 'flight_search.html')
