d516a5844e1642400bd589f72e6e5f169505cde2
Murphy Schaff <murphy.schaff@gmail.com>
2024-03-07 16:24:42 -0700

    made changes to flight search, added compare option header

--- flightcomparison/views.py
+++ flightcomparison/views.py
@@ -18,19 +18,31 @@ def home(request):
   return render(request, 'home.html', {'flights': data['data']})
 
 def flight_search(request):
-    if request.method == 'GET':
-        departure_location = request.GET.get('departure_location', '')
-        departure_time = request.GET.get('departure_time', '')
-        price = request.GET.get('price', '')
+    response = serializers.FlightListView.get(request)
+    binary = response.content
+    data = json.loads((binary).decode())
+    return render(request, 'flight_search_blank.html', {'flights': data['data']})
 
-        flights = Flight.objects.all()
-        if departure_location:
-            flights = flights.filter(departure_location__icontains=departure_location)
-        if departure_time:
-            flights = flights.filter(departure_time=departure_time)
-        if price:
-            flights = flights.filter(price=price)
+def flight_search_data(request): 
+    departure_location = request.GET.get('departure_location', '')
+    departure_time = request.GET.get('departure_time', '')
+    price = request.GET.get('price', '')
 
-        return render(request, 'flight_search.html', {'flights': flights})
-    else:
-        return render(request, 'flight_search.html')
+    flights = Flight.objects.all()
+    if departure_location:
+        flights = flights.filter(departure_location__icontains=departure_location)
+    if departure_time:
+        flights = flights.filter(departure_time=departure_time)
+    if price:
+        flights = flights.filter(price=price)
+
+    return render(request, 'flight_search_data.html', {'flights': flights})
+
+
+'''
+Compare views (for flight comparisons)
+'''
+#have no flights to compare to
+def compare(request):
+    return render(request, 'compare/compare_blank.html')
+#Have one flight to compare to
