b9204cb7b1fdb00d9f41cb539d558bcea4a66efe
rbieg <rbieg@uccs.edu>
2024-03-31 17:00:29 -0600

    added to urls and still works

--- flightcomparison/urls.py
+++ flightcomparison/urls.py
@@ -11,4 +11,14 @@ urlpatterns = [
     path('compare/<int:flight_1_id>/<int:flight_2_id>/<str:sort>', views.compare, name="compare"),
     path('flights/', views.FlightList.as_view(), name='flights'),
     path('flight/<int:pk>/', views.FlightDetail.as_view(), name='flight_detail'),
+    
+    # # Added URL patterns for API methods
+    # path('api-calls/', TemplateView.as_view(template_name='api_calls.html'), name='api_calls'),
+    # path('get-arrivals-by-airport/', views.get_arrivals_by_airport, name='get_arrivals_by_airport'),
+    # path('get-departures-by-airport/', views.get_departures_by_airport, name='get_departures_by_airport'),
+    # path('get-flights-by-aircraft/', views.get_flights_by_aircraft, name='get_flights_by_aircraft'),
+    # path('get-flights-from-interval/', views.get_flights_from_interval, name='get_flights_from_interval'),
+    # path('get-my-states/', views.get_my_states, name='get_my_states'),
+    # path('get-states/', views.get_states, name='get_states'),
+    # path('get-track-by-aircraft/', views.get_track_by_aircraft, name='get_track_by_aircraft'),
 ]
