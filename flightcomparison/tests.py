73d2af2cb15dc06ea0f9b9e3749bff46897cabbe
Murphy Schaff <murphy.schaff@gmail.com>
2024-04-15 10:44:37 -0600

    fixed and added test, moved autocomplete.js to its own file

--- flightcomparison/tests.py
+++ flightcomparison/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth.models import User
 from django.utils import timezone
-from .models import Flight
+from .models import Flight, FlightData
 
 class FlightModelTests(TestCase):
     def setUp(self):
@@ -15,6 +15,13 @@ class FlightModelTests(TestCase):
             departure_location="City A",
             arrival_location="City B",
             departure_time=timezone.now(),
+            departure_location_latitude = 1,
+            departure_location_longitude = 1,
+            layover_location = "City C",
+            layover_location_latitude = 1,
+            layover_location_longitude = 1,
+            arrival_location_latitude = 1,
+            arrival_location_longitude = 1,
             arrival_time=timezone.now(),
             price=100,
             seat_number=1
@@ -24,21 +31,46 @@ class FlightModelTests(TestCase):
         # Tests the __str__ method returns expected string format.
         self.assertEqual(flight.__str__(), 'testuser: City A to City B')
 
-class HomeViewTests(TestCase):
+    def test_FlightData_creation(self):
+        flight = FlightData.objects.create(
+            icao24 = "CODE",
+            firstSeen = 1234,
+            estDepartureAirport = "KDEN",
+            lastSeen = 1,
+            estArrivalAirport = "KGRB",
+            callsign = "SIGN",
+
+            estDepartureAirportHorizDistance = 1,
+            estDepartureAirportVertDistance = 1,
+            estArrivalAirportHorizDistance = 1,
+            estArrivalAirportVertDistance = 1,
+            departureAirportCandidatesCount = 1,
+            arrivalAirportCandidatesCount = 1
+        )
+
+        #asserting the important data is correct
+        self.assertEqual(flight.estDepartureAirport, "KDEN")
+        self.assertEqual(flight.estArrivalAirport, "KGRB")
+        self.assertEqual(flight.firstSeen, 1234)
+
+        #testing functions related to model
+        self.assertEqual(flight.__str__(), "CODE: KDEN KGRB")
+
+class UserInterfaceTests(TestCase):
     '''
     Home View
     '''
-    def test_home_view_status_code(self):
+    def test_recommend_view_status_code(self):
         # Tests that the home view returns a 200 status code.
-        url = reverse('home')
+        url = reverse('recommend')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
-    def test_home_view_uses_correct_template(self):
+    def test_recommend_view_uses_correct_template(self):
         # Tests that the home view uses the correct template.
-        url = reverse('home')
+        url = reverse('recommend')
         response = self.client.get(url)
-        self.assertTemplateUsed(response, 'flightcomparison/home.html')
+        self.assertTemplateUsed(response, 'flightcomparison/recommend.html')
 
     '''
     Flight_Search view
@@ -72,12 +104,19 @@ class HomeViewTests(TestCase):
             user=self.user,
             departure_location="City A",
             arrival_location="City B",
+            departure_location_latitude = 1,
+            departure_location_longitude = 1,
+            layover_location = "City C",
+            layover_location_latitude = 1,
+            layover_location_longitude = 1,
+            arrival_location_latitude = 1,
+            arrival_location_longitude = 1,
             departure_time=timezone.now(),
             arrival_time=timezone.now(),
-            price=100,
+            price=100, 
             seat_number=1
         )
-        url = reverse('compare', kwargs={'flight_1_id': 1, 'flight_2_id': 1, 'sort': 'price'})
+        url = reverse('compare/list', kwargs={'flight_1_id': 1, 'flight_2_id': 1, 'sort': 'price'})
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
@@ -88,18 +127,25 @@ class HomeViewTests(TestCase):
             user=self.user,
             departure_location="City A",
             arrival_location="City B",
+            departure_location_latitude = 1,
+            departure_location_longitude = 1,
+            layover_location = "City C",
+            layover_location_latitude = 1,
+            layover_location_longitude = 1,
+            arrival_location_latitude = 1,
+            arrival_location_longitude = 1,
             departure_time=timezone.now(),
             arrival_time=timezone.now(),
             price=100,
             seat_number=1
         )
         #tests to make sure the flight_compare template is being used
-        url = reverse('compare', kwargs={'flight_1_id': 1, 'flight_2_id': 1, 'sort': 'price'})
+        url = reverse('compare/list', kwargs={'flight_1_id': 1, 'flight_2_id': 1, 'sort': 'price'})
         response = self.client.get(url)
         self.assertTemplateUsed(response, 'flightcomparison/flight_compare.html')
     
 
-class FlightSearchViewTests(TestCase):
+class FlightSearchTests(TestCase):
     def setUp(self):
         # Creates a Flight instance for testing the search functionality.
         self.user = User.objects.create_user(username='testuser', password='12345')
@@ -107,6 +153,13 @@ class FlightSearchViewTests(TestCase):
             user=self.user,
             departure_location="City A",
             arrival_location="City B",
+            departure_location_latitude = 1,
+            departure_location_longitude = 1,
+            layover_location = "City C",
+            layover_location_latitude = 1,
+            layover_location_longitude = 1,
+            arrival_location_latitude = 1,
+            arrival_location_longitude = 1,
             departure_time=timezone.now(),
             arrival_time=timezone.now(),
             price=100,
@@ -115,7 +168,7 @@ class FlightSearchViewTests(TestCase):
 
     def test_search_by_departure_location(self):
         # Tests flight search functionality filters by departure location.
-        response = self.client.get(reverse('flight_search'), {'departure_location': 'City A'})
+        response = self.client.get(reverse('flight_search_data'), {'departure_location': 'City A'})
         self.assertContains(response, "City A")
         self.assertNotContains(response, "No flights found.")
-        
+
